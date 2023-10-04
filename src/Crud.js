f8236c2b5db841adb9f44485008d8888495a3e7b
stella <stellamaia97@hotmail.com>
2023-10-04 12:29:23 -0300

    Implementação de component

---
+++ src/Crud.js
@@ -0,0 +1,15 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import axios from 'axios'
+import NavBar from "./NavBar";
+
+function Crud() {
+    return (
+        <div>
+            <NavBar />
+        {/* Renderizar formulário de edição/criação aqui */}
+        <button onClick={handleSubmit}>Salvar</button>
+        <button onClick={handleDelete}>Excluir</button>
+        </div>
+    )
+}
+export default Crud;
