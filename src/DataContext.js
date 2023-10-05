0b41a14df1d47491cd1ff5e8ef8af3864b668054
stella <stellamaia97@hotmail.com>
2023-10-05 17:16:22 -0300

    Modificações no texto

--- src/DataContext.js
+++ src/DataContext.js
@@ -17,12 +17,12 @@ export const DataProvider = ({ children }) => {
     //Exporta um componente chamado DataProvider, responsável por carregar os dados iniciais da API 
     //e disponibilizá-los para outros componentes filhos. Ele recebe children como propriedade
     //que representa os componentes filhos encapsulados por ele.
-    const [task, setTask] = useState([]);
+    const [tasks, setTasks] = useState([]);
     const [users, setUsers] = useState([]);
     //Usa o hook 'useState' para criar dois estados locais, 'tasks' e 'users'
     //Esses estados são usados para armazenar os dados obtidos da API.
     //Inicialmente ambos estão vazios.
-}
+
 
 
 //Você usa o 'useEffect' paraq fazer duas solicitações HTTP à API ao montar o component
@@ -33,7 +33,7 @@ useEffect(() => {
     //Busca as tarefas da API
     axios.get('https://jsonplaceholder.typicode.com/todos')
         .then((response) => {
-            setTask(response.data)
+            setTasks(response.data)
         })
         .catch((error) => {
             console.log('Erro ao buscar tarefas', error);
@@ -51,46 +51,38 @@ useEffect(() => {
 //Serão usadas para manipular tarefas na API e atualizar o estado local.
 const addTask = (newTask) => {
     //Lógica para adicionar tarefa na API e atualizar o estado.
-    setTask([...task, newTask]);
+    setTasks([...tasks, newTask]);
 };
 //const seria o metodo em vue
 const editTask = (taskId, updatedTask) => {
     //Lógica para editar tarefa na API e atualizar o estado.
-    setTask(task.map((task) => task.id === taskId ? updatedTask : task));
+    setTasks(tasks.map((task) => task.id === taskId ? updatedTask : task));
 };
 const deleteTask = (taskId) => {
     //Lógica para deletar tarefa na API e atualizar o estado.
-    setTask(task.filter((task) => task.id !== taskId));
+    setTasks(tasks.filter((task) => task.id !== taskId));
 
 }
-return (
+const contextValue = {
+    tasks,
+    users,
+    addTask,
+    editTask,
+    deleteTask,
+  };
     //Retorna o contexto dos dados(DataContext.Provider) envolvendo todos os componentes filhos
     //e passa um objeto como valor do contexto, que inclui os estados 'tasks' , 'users'
     // e as funções  de gerenciamento de tarefas.
     //Isso tornará esses valores e funções acessíveis  a partir de qualquer componentes que consuma este contexto.
-    <DataContext.Provider value={{ task, users, addTask, editTask, deleteTask }}>
-        {children}
-    </DataContext.Provider>
-
+    return (
+        <DataContext.Provider value={contextValue}>
+          {children}
+        </DataContext.Provider>
+      );
     //configurou um contexto de dados que pode ser usado para compartilhar informações 
     //e funcionalidades relacionadas a tarefas e usuários em toda a sua aplicação React.
     // Componentes podem acessar essas informações e funções usando o useData()
     // exportado. Certifique-se de importar e usar o DataProvider em torno dos componentes
     // que precisam acessar esse contexto.
-)
-
-
-
-
-
-
-
-// function DataContext() {
-//     return (
-//         <div>
-//         <p>DataContext</p>
-
-//         </div>
-//     )
-// }
+    };
 export default DataContext;
