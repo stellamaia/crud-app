76e2b5c356c13fe0810eac3ca8a51a0d42788bb0
stella <stellamaia97@hotmail.com>
2023-10-07 01:09:12 -0300

    Estilização de lista

--- src/UserList.js
+++ src/UserList.js
@@ -8,35 +8,45 @@ const UserList = () => {
   return (
     <div>
       <h2>Usuários</h2>
-      <ul className='container-ul'>
+      <div className='row'>
         {users.map((user) => (
-          <li key={user.id} className='container'>
-            <p>
-              <strong>Nome:</strong> {user.name}
-            </p>
-            <p>
-              <strong>Sobrenome:</strong> {user.username}
-            </p>
-            <p>
-              <strong>Email:</strong> {user.email}
-            </p>
-            <p>
-              <strong>Endereço:</strong> {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}
-            </p>
-            <p>
-              <strong>Telefone:</strong> {user.phone}
-            </p>
-            <p>
-              <strong>Site web:</strong> {user.website}
-            </p>
-            <p>
-              <strong>Empresa:</strong> {user.company.name}
-            </p>
-          </li>
+
+
+
+          <div key={user.id} className='container col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2'>
+
+            <ul className='container-ul'>
+            <li>
+              <p>
+                <strong>Nome:</strong> {user.name}
+              </p>
+              <p>
+                <strong>Sobrenome:</strong> {user.username}
+              </p>
+              <p>
+                <strong>Email:</strong> {user.email}
+              </p>
+              <p>
+                <strong>Endereço:</strong> {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}
+              </p>
+              <p>
+                <strong>Telefone:</strong> {user.phone}
+              </p>
+              <p>
+                <strong>Site web:</strong> {user.website}
+              </p>
+              <p>
+                <strong>Empresa:</strong> {user.company.name}
+              </p>
+            </li>
+            </ul>
+          </div>
+      
+   
+  
         ))}
-      </ul>
-    
     </div>
+    </div >
   );
 };
 
